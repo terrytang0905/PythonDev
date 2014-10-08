8e9bbc3e596ae45925999474e291cf750adfeea4
terrytang0905 <zhenjietang@hotmail.com>
2014-10-08 21:29:33 +0800

    test

--- PythonTool/com/ttsw/basic/helloWorld.py
+++
@@ -1,7 +0,0 @@
-
-print("hello world!")
-
-i= 5 
-print i
-i=i+1
-print i
--- PythonTool/com/ttsw/project/assert.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Jun 4, 2014
-
-@author: terrymac
-'''
-
-mylist=['item']
-assert len(mylist) >= 1
-print mylist.pop()
